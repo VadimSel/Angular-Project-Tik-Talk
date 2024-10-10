3b6823869d63b4790d92bde461e6b5c6a070c9ec
VadimSel <selyaaa33@gmail.com>
2024-10-10 20:04:16 +0300

    Added fonts and worked with the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { Profile } from "./data/interfaces/profile.interface";
+import { ProfilesService } from './data/services/profiles.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-tik-talk';
+  profileService = inject(ProfilesService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val
+    });
+  }
 }
