6a3c74d43438662e1e39f81e96680774bd8c125c
VadimSel <selyaaa33@gmail.com>
2024-10-12 14:31:16 +0300

    added better autorization with saving token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,18 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { TokenResponse } from '../app.types';
 import { Observable, tap } from 'rxjs';
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
+  cookieService = inject(CookieService)
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
 
   token: string | null = null
   refreshToken: string | null = null
 
   get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token')
+    }
     return !!this.token
   }
 
@@ -31,6 +36,9 @@ export class AuthService {
         tap(val => {
           this.token = val.access_token
           this.refreshToken = val.refresh_token
+
+          this.cookieService.set('token', this.token)
+          this.cookieService.set('refreshToken', this.refreshToken)
         })
       )
   }
